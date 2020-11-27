ef814906640c3410a0b936eee81f60074ae6094f
hrudesh-lenka <hrudesh.lenka@gmail.com>
2020-11-27 22:36:07 +0530

    added routes

--- server.js
+++ server.js
@@ -20,9 +20,19 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let date_string = req.params.date_string;
+  const date = new Date(date_string);
+  if(date.toString() == "Invalid Date"){
+    res.json({ error : "Invalid Date" });
+  }
+  res.json({unix: date.getTime() , utc:date.toUTCString()});
 });
+
+app.get("/api/timestamp/", function (req, res){
+  const date = new Date();
+  res.json({unix:date.getTime() , utc:date.toUTCString()})
+})
 
 
 
